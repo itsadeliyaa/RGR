327bd62df03157f08fbd15ba919f0e9c19541a81
itsadeliyaa <44906572+itsadeliyaa@users.noreply.github.com>
2018-12-14 20:14:18 +0500

    Update DepositCalculatorTest.java

--- RG/src/DepositCalculatorTest.java
+++ RG/src/DepositCalculatorTest.java
@@ -8,7 +8,7 @@ public class DepositCalculatorTest {
 	public void testDeposit() {
 		DepositCalculator test = new DepositCalculator();
 		double result = test.Deposit(60000.0 , 11.0, 5);
-		assertEquals(93000, result,0);
+		assertEquals(93000, result,0.1);
 	}
 
 
@@ -16,6 +16,6 @@ public class DepositCalculatorTest {
 	public void testDeposit1() {
 		DepositCalculator test = new DepositCalculator();
 		double result = test.Deposit(16000.0 , 9.0, 3);
-		assertEquals(20320, result,0);
+		assertEquals(20320, result,0.1);
 	}
 }
